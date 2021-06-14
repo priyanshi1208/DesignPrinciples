33decb64329c251e80a9db92df6ce387de177dc1
priyanshi1208 <pri.shukla2208@gmail.com>
2021-06-14 15:55:42 +0530

    Added:new test cases to check if custom exception is arising

--- src/test/java/com/magic/csvdesignprinciple/CSVStateCensusAnalyzerTest.java
+++ src/test/java/com/magic/csvdesignprinciple/CSVStateCensusAnalyzerTest.java
@@ -1,11 +1,24 @@
 package com.magic.csvdesignprinciple;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+
 public class CSVStateCensusAnalyzerTest {
     @Test
-    public void validating_if_stateCensus_records_matches_no_of_entries() {
+    public void validating_if_stateCensus_records_matches_no_of_entries() throws CustomExceptionsCsvFile {
         CSVStateCensusAnalyzer csvStateCensusAnalyzer=new CSVStateCensusAnalyzer();
         int fromCSVfile = csvStateCensusAnalyzer.loadCensusDataFromCSVfile("C:\\Users\\ayuanshi\\Downloads\\StateCensusData.csv");
         Assert.assertEquals(29,fromCSVfile);
+    }
+
+    @Test
+    public void validating_if_exception_arises_and_handles_if_no_such_file_exists()throws CustomExceptionsCsvFile {
+       try {
+           CSVStateCensusAnalyzer csvStateCensusAnalyzer = new CSVStateCensusAnalyzer();
+           csvStateCensusAnalyzer.loadCensusDataFromCSVfile("C:\\Users\\ayuanshi\\Downloads\\StateCensusData1.csv");
+       }catch(CustomExceptionsCsvFile e){
+        Assert.assertEquals(CustomExceptionsCsvFile.ExceptionType.Invalid_FilePath,e.exceptionType);
+       }
     }
 }
